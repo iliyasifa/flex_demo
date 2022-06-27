f06b940e848e0ee4deb6ab2d72ea58a43d82677b
IFAMac <iliyasifa@gmail.com>
2022-06-27 15:45:55 +0530

    commit on master

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -29,6 +31,8 @@ class HomeWidget extends StatefulWidget {
 class _HomeWidgetState extends State<HomeWidget> {
   bool _vertical = true;
   int _alignTextIndex = 0;
+  int colorIndex = 0;
+  int colrListIndex = 0;
 
   final List<MainAxisAlignment> _alignment = [
     MainAxisAlignment.start,
@@ -47,86 +51,103 @@ class _HomeWidgetState extends State<HomeWidget> {
     'spaceEvenly',
   ];
 
+  var colrs = [Colors.purple, Colors.red, Colors.blue];
+
   @override
   Widget build(BuildContext context) {
+    var appBarActions = [
+      IconButton(
+        onPressed: () {
+          setState(() {
+            _vertical = !_vertical;
+          });
+        },
+        tooltip: 'Axis direction',
+        icon: const Icon(
+          Icons.rotate_right,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Text(_vertical ? 'Vertical' : 'Horizontal'),
+      ),
+      IconButton(
+        onPressed: () {
+          setState(() {
+            _alignTextIndex++;
+            if (_alignTextIndex >= _alignment.length) {
+              _alignTextIndex = 0;
+            }
+          });
+        },
+        tooltip: 'alignment',
+        icon: const Icon(
+          Icons.aspect_ratio,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Text(_alignmentText[_alignTextIndex]),
+      ),
+      const Padding(
+        padding: EdgeInsets.all(5),
+      ),
+    ];
+    RawMaterialButton purpleButton = RawMaterialButton(
+      onPressed: () {},
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18),
+      ),
+      fillColor: colrs[colorIndex],
+      child: const Text(
+        'One',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+    RawMaterialButton greyButton = RawMaterialButton(
+      onPressed: () {},
+      shape: const CircleBorder(),
+      fillColor: colrs[colorIndex],
+      child: const Text(
+        'Ywo',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+    RawMaterialButton amberButton = RawMaterialButton(
+      onPressed: () {},
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18),
+      ),
+      fillColor: colrs[colorIndex],
+      child: const Text(
+        'Three',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flex Demo'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              setState(() {
-                _vertical = !_vertical;
-              });
-            },
-            tooltip: 'Axis direction',
-            icon: const Icon(
-              Icons.rotate_right,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(_vertical ? 'Vertical' : 'Horizontal'),
-          ),
-          IconButton(
-            onPressed: () {
-              setState(() {
-                _alignTextIndex++;
-                if (_alignTextIndex >= _alignment.length) {
-                  _alignTextIndex = 0;
-                }
-              });
-            },
-            tooltip: 'alignment',
-            icon: const Icon(
-              Icons.aspect_ratio,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(_alignmentText[_alignTextIndex]),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(5),
-          ),
-        ],
+        actions: appBarActions,
       ),
       body: Flex(
         direction: _vertical ? Axis.vertical : Axis.horizontal,
         mainAxisAlignment: _alignment[_alignTextIndex],
         children: [
-          RawMaterialButton(
-            onPressed: () {},
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18),
-            ),
-            fillColor: Colors.purple,
-            child: const Text(
-              'One',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          RawMaterialButton(
-            onPressed: () {},
-            shape: const CircleBorder(),
-            fillColor: Colors.grey,
-            child: const Text(
-              'Ywo',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          RawMaterialButton(
-            onPressed: () {},
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18),
-            ),
-            fillColor: Colors.amber,
-            child: const Text(
-              'Three',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          purpleButton,
+          greyButton,
+          amberButton,
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh),
+        onPressed: () {
+          setState(() {
+            colorIndex++;
+            if (colorIndex >= colrs.length) {
+              colorIndex = 0;
+            }
+          });
+        },
       ),
     );
   }
